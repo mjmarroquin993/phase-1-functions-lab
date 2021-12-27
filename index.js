3999e292d2773e90b556ff758bd06190a9ed5f3d
Josh Marroquin <mj.marroquin993@gmail.com>
2021-12-26 16:02:19 -0800

    commit tree 9029bf8182cafcae0da27d34364d6ad48a7f1024

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
-function distanceFromHqInBlocks(someValue){
-    console.log()
-
+function distanceFromHqInBlocks(someValue) {
+    if (someValue > 42) {
+        return someValue - 42
+    }
+    else {
+        return 42 - someValue
+    }
 }
 
-function distanceaFromHqInFeet(someValue){
-    console.log
-
+function distanceFromHqInFeet(someValue) {
+    return distanceFromHqInBlocks(someValue) * 264
 }
+
+function distanceTravelledInFeet(start, destination) {
+    return Math.abs(destination - start) * 264
+}
+
+function calculatesFarePrice(start, destination) {
+    const distance = distanceTravelledInFeet(start, destination);
+
+    if (distance <= 400) {
+      return 0;
+    } else if (distance > 400 && distance <= 2000) {
+      return .02 * (distance - 400);
+    } else if (distance > 2000 && distance < 2500) {
+      return 25;
+    } else {
+      return 'cannot travel that far';
+    }
+  } 
